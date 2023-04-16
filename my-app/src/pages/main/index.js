b12d860ed8006eeef8f51d7959872d8013424a22
YeagerKing <bennikang@outlook.com>
2023-04-16 16:02:54 +0800

    frontend commit

--- my-app/src/pages/main/index.js
+++ my-app/src/pages/main/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { useNavigate  } from 'react-router-dom';
-import {  Button, Card} from 'antd'
+import {  Card} from 'antd'
 import "./index.css";
 function Component1() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   // const handleClick_kelly = () => { navigate('/kelly');window.location.reload();};
-  const handleClick_marketnew = () => { navigate('/marketnew');window.location.reload();};
-  const handleClick_compunds = () => { navigate('/compound');window.location.reload();};
-  const handleClick_simple = () => { navigate('/simple');window.location.reload();};
+  // const handleClick_marketnew = () => { navigate('/marketnew');window.location.reload();};
+  // const handleClick_compunds = () => { navigate('/compound');window.location.reload();};
+  // const handleClick_simple = () => { navigate('/simple');window.location.reload();};
 
   return (
     <div className="login">
@@ -18,18 +17,22 @@ function Component1() {
         </div>
         
         <div className="calculator-item-row">
-          <li><a className="gradient-button gradient-button-1" href='/kelly'>跳转到 kelly-formula</a></li>
-          <li><a className="gradient-button gradient-button-1" href='/kelly'>跳转到 kelly-formula</a></li>
-          <li><a className="gradient-button gradient-button-1" href='/kelly'>跳转到 kelly-formula</a></li>
-          <li><a className="gradient-button gradient-button-1" href='/kelly'>跳转到 kelly-formula</a></li>
-          
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/kelly'>Kelly Calculator</a></li></div>
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/compound'>Compound Interest</a></li></div>
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/simple'>Simple Interest</a></li></div>
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/marketnews'>Marketnews</a></li></div>
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/loans'>Loans</a></li></div>
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/loanseuqal'>Loans equal</a></li></div>
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/kelly'>xxx</a></li></div>
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/kelly'>xxx</a></li></div>          
+          <div className="bottom-container"><li><a className="gradient-button gradient-button-1" href='/kelly'>xxx</a></li></div>          
         </div>
           
-        <Button onClick={handleClick_marketnew}>跳转到 marketnew</Button>
+        {/* <Button onClick={handleClick_marketnew}>跳转到 marketnew</Button>
         <Button onClick={handleClick_compunds}>跳转到 compound</Button>
         <Button onClick={handleClick_simple}>跳转到 simple</Button>
         <Button onClick={handleClick_marketnew}>跳转到 Component2</Button>
-        <Button onClick={handleClick_compunds}>跳转到 Component2</Button>
+        <Button onClick={handleClick_compunds}>跳转到 Component2</Button> */}
       </Card>
     </div>
   );
