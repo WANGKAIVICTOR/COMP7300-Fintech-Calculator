be5d0f6730bff7721e1e9e605a9b81b024ad15f6
Victor <22436022@life.hkbu.edu.hk>
2023-04-16 17:46:24 +0800

    update the comments

--- functions.py
+++ functions.py
@@ -16,24 +16,24 @@ def market_news(category="forex"):
 def kelly(b, p, q):
     """
     https://wiki.mbalib.com/wiki/%E5%87%AF%E5%88%A9%E5%85%AC%E5%BC%8F
-    凯利公式是一条可应用在投资资金和赌注的公式。
-    应用于多次的随机赌博游戏，资金的期望增长率最高，
-    且永远不会导致完全损失所有资金的后果。它假设赌博可无限次进行，而且没有下注上下限。
-    这条公式是克劳德·艾尔伍德·香农在贝尔实验室的同事物理学家约翰·拉里·凯利在1956年提出的。
-    凯利的方法参考了香农关于长途电话线的嘈音的工作。凯利说明香农的信息论可应用于此：
-    赌徒不必要获得完全的资讯。香农的另一位同事Edward O. Thorp应用这条公式在廿一点和股票市场上。
-    1738年丹尼·伯努利曾提出等价的观点，可是伯努利的文章直到1954年才首次译成英语。
-    不过对于只投资一次的人来说，应选择算术平均最高的投资组合。
+    The Kelly formula is a formula that can be applied to investment funds and bets.
+    Applied to multiple random gambling games, the expected growth rate of funds is the highest,
+    And will never result in a complete loss of all funds. It assumes an infinite number of bets, with no upper or lower limits.
+    The formula was proposed in 1956 by physicist John Larry Kelly, a colleague of Claude Ellwood Shannon at Bell LABS.
+    Kelly's approach draws on Shannon's work on the rustle of long-distance telephone lines. Kelly shows that Shannon's information theory can be applied here:
+    Gamblers do not need complete information. Another of Shannon's colleagues, Edward O. Thorp, applied this formula to the blackjack and stock markets.
+    An equivalent idea was proposed by Danny Bernoulli in 1738, but Bernoulli's work was not first translated into English until 1954.
+    But for a one-time investor, choose the portfolio with the highest arithmetic average.
     """
 
     """
-    例如：若一个游戏有40%（p=0.40）机会胜出，赔率为2:1（b=2），这个赌客便应每次投注(2 × 0.40 -0.60)/2 = 10%的资金。
+    For example, if a game has a 40% (p=0.40) chance of winning and the odds are 2:1 (b=2), the gambler should bet (2 × 0.40-0.60)/2 = 10% of the money each time.
 
-    再举个最简单的例子：硬币的正反面，正面胜，反面输，胜率50%，赔率1.5:1。
+    Let's take the simplest example: heads win, tails lose, 50 percent, 1.5:1.
 
-    代入凯利公式，b赔率是1.5，p和q都是0.5，则f=(bp - q) ÷ b = (1.5 * 50% - 50%) ÷ 1.5 = 16.6%。
+    Plug into Kelly's formula, b's odds are 1.5, p and q are both 0.5, then f=(bp-q) ÷ b =(1.5 * 50%-50%) ÷ 1.5 = 16.6%.
 
-    f^*=16.6%是你下注最有利的比例，每次拿出16.6%进行下注，才能使你的收益最大化。 这是在你期望值为正的前提下，即（bp-q）>0，在抛硬币的案例里，期望值=0.25（赢面）是正的。
+    f^*=16.6% is the most favorable percentage you can bet, and 16.6% is the percentage you can bet each time to maximize your returns. And that's assuming you have a positive expected value, which is bp-q. 0. In the coin toss case, the expected value =0.25 is positive.
     """
     return round((b*p-q)/b, 6)
 
@@ -41,18 +41,18 @@ def kelly(b, p, q):
 def mixed_simple_interest(k, y, m, d, i):
     """
     https://wiki.mbalib.com/wiki/%E5%8D%95%E5%88%A9 
-    单利指借款的成本或放贷的收益，是计算利息的一种方法，单利的计算取决于所借款项或供款的金额本金，资余借用时间的长及市场一利率水平等因素
-    按照单利计算的方法，只要本金在贷款期限中获得利息，不管时间多长，所生利息均不加人本全重复计算利息。这里所说的"本金”是指货给别人以收取利息的原本全额，“利息”是指借款人付给贷款人超过本金部分的金额
-    期限不是整数年的情况，比如储户的存款期限为 3 年，7 个月另 12 天，这时的收益的计算方法仍然按照上面的公式，但是要把月和天数换算成年数。在经济数学领域的日期换算中遵照如下约定：1 年有 360 天，1 个月有 30 天。
+    Simple interest refers to the cost of borrowing money or the return on lending money. Simple interest is a method of calculating interest. The calculation of simple interest depends on factors such as the principal amount of the loan or contribution, the length of time the balance is borrowed and the level of market interest rate
+    Under the simple interest method, as long as the principal earns interest over the life of the loan, no matter how long it is, the interest accrues without any human interest. "Principal" here refers to the original full amount of the goods given to another person for interest, and "interest" refers to the amount the borrower pays the lender in excess of the principal amount
+    If the term is not a round number of years, for example, the depositor has a term of 3 years, 7 months and 12 days, then the return will be calculated according to the above formula, but the months and days will be converted into adult numbers. The date conversion in the field of economics and mathematics follows the following convention: there are 360 days in a year and 30 days in a month.
     Para:
-        k: 本金
-        y: 年份
-        m: 月份
-        d: 天数
-        i: 年利率
+        k: principal
+        y: years
+        m: months
+        d: days
+        i: Annual interest rate
     Return:
-        interest: 利息
-        total: 总计返还
+        interest: interest
+        total: Total return
     """
     interest = k*(y+m/12+d/360)*i
     total = k+interest
@@ -61,16 +61,16 @@ def mixed_simple_interest(k, y, m, d, i):
 
 def mixed_compound_interest(k, y, m, d, i):
     """
-    复利就是复合利息，它是指每年的收益还可以产生收益，具体是将整个借贷期限分割为若干段，前一段按本金计算出的利息要加入到本金中，形成增大了的本金，作为下一段计算利息的本金基数，直到每一段的利息都计算出来，加总之后，就得出整个借贷期内的利息，简单来说就是俗称的利滚利。
+    Compound interest is compound interest, which means that annual income can also generate income. Specifically, the whole loan term is divided into several sections, and the interest calculated according to the principal of the previous section should be added into the principal to form the increased principal, which will serve as the principal base for the calculation of interest of the next section until the interest of each section is calculated. After the sum, the interest of the whole loan period can be obtained. Simply put, it is commonly known as compound interest.
     Para:
-        k: 本金
-        y: 年份
-        m: 月份
-        d: 天数
-        i: 年利率
+        k: principal
+        y: years
+        m: months
+        d: days
+        i: Annual interest rate
     Return:
-        interest: 利息
-        total: 总计返还
+        interest: interest
+        total: Total return
     """
     total = k*(1+i)**y*(1+i*30*m*d/360)
     interest = total - k
@@ -79,17 +79,17 @@ def mixed_compound_interest(k, y, m, d, i):
 
 def loan_principal_equal(k, y, t, i):
     """
-    等额本金
-    日利率=年利率÷360=月利率÷30
+    Equivalent principal
+    Daily interest rate = annual interest rate ÷360= monthly interest rate ÷30
     Para:
-        k: 本金
-        y: 年份
-        t: 分期数量
-        i: 年利率
+        k: principal
+        y: years
+        t: Staging quantity
+        i: Annual interest rate
     Return:
-        interest: 总利息
-        total: 总还款额
-        each_time: 每次还款的金额
+        each_time: Amount of each repayment
+        total: Total payment
+        interest: interest
     """
     rate = i/(t/y)
     Monthly_principal = k/t
@@ -104,17 +104,17 @@ def loan_principal_equal(k, y, t, i):
 
 def loans_equal_principal_and_interest(k, y, t, i):
     """
-    等额本息
-    日利率=年利率÷360=月利率÷30
+    Equal principal and interest
+    Daily interest rate = annual interest rate ÷360= monthly interest rate ÷30
     Para:
-        k: 本金
-        y: 年份
-        t: 分期数量
-        i: 年利率
+        k: principal
+        y: years
+        t: Staging quantity
+        i: Annual interest rate
     Return:
-        each_time: 每次还款金额
-        total: 总还款额
-        interest: 利息
+        each_time: Amount of each repayment
+        total: Total payment
+        interest: interest
     """
     rate = i/(t/y)
     # 贷款本金×[月利率×(1+月利率) ^ 还款月数]÷{[(1+月利率) ^ 还款月数]-1}
