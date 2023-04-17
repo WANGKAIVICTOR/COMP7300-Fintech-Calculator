813f6704be671f4fb5a262b21be94b960a63e464
Victor <22436022@life.hkbu.edu.hk>
2023-04-18 01:48:03 +0800

    finish chatbot

--- my-app/src/pages/chat.js
+++ my-app/src/pages/chat.js
@@ -1,25 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Widget, addResponseMessage } from 'react-chat-widget';
 
 import 'react-chat-widget/lib/styles.css';
 
 function Chat() {
+    const [history, setHistory] = useState([])
+
     useEffect(() => {
         addResponseMessage('Welcome to this **awesome** chat!');
     }, []);
 
     const handleNewUserMessage = (newMessage) => {
         console.log(`New message incoming! ${newMessage}`);
-        // Now send the message throught the backend API
+        fetch('https://c555-35-197-145-163.ngrok.io/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                mode: 'cors',
+                heads: { 'content-type': 'application/x-www-form-urlencoded' }
+            },
+            body: JSON.stringify({ "prompt": newMessage, "history": history })
+        })
+            .then(response => response.json())
+            .then(resp => setHistory(resp['history']))
+            .then(resp => addResponseMessage(resp['response']))
+            .catch(error => console.log(error))
     };
 
     return (
         <div className="App">
             <Widget
                 handleNewUserMessage={handleNewUserMessage}
-                // profileAvatar={logo}
-                title="My new awesome title"
-                subtitle="And my cool subtitle"
+                title="Fintech Chatbot"
+                subtitle="Welcome to CHATGLM"
             />
         </div>
     );
